88745035d5fb733a6d473fbd6d4a4acbfccff3f0
Korir Carlos <carlkorir226@gmail.com>
2025-03-08 22:44:21 +0300

    adds advanced functions

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solution in this file!
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+
+ const returnFirstTwoDrivers = function(){
+    return drivers.slice(0, 2)
+ }
+
+ const returnLastTwoDrivers = function (){
+    return drivers.slice(-2,)
+ }
+
+ const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+ function createFareMultiplier (fare){
+    return function (multiplier){
+        return fare * multiplier
+    }
+  }
+const fareDoubler = function(fare){
+    return fare * 2
+}
+const fareTripler = function(fare){
+    return fare * 3
+}  
+
+function selectDifferentDrivers(drivers, choose){
+  return choose(drivers)
+}
